8c884208954be4c1ec17210db8abff15ea3de64f
Vitaly Hutarau <vitaliken@gmail.com>
2023-03-12 21:52:06 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const addItems = document.querySelector('.add-items'); // query by class name
+const itemsList = document.querySelector('.plates'); // query by class name
+
+addItems.addEventListener('submit', addItem); // run addItem callback fntn at each submit.
